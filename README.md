b7d5afb2341291d0269c53db39c49b1d18ed8847
Dimitar Gaydardzhiev <dimiter.gg@gmail.com>
2022-11-28 18:19:22 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Stack-vs-Heap-memory-in-CSharp
+
+An example application related to the following blog post https://dicodeplace.com/stack-vs-heap-in-c-sharp
---
+++ StackVsHeap/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StackVsHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            var address = GetObjectMemoryAddress(number);
+            Console.WriteLine($"Stack address of the reference type {typeof(int).Name}: {address}");
+
+            Point notDefinedPoint = null;
+            address = GetObjectMemoryAddress(notDefinedPoint);
+            Console.WriteLine($"Stack address of the reference type {typeof(Point).Name}: {address}");
+
+            Point definedPoint = new Point();
+            address = GetObjectMemoryAddress(definedPoint);
+            Console.WriteLine($"Stack address of the reference type {typeof(int).Name}: {address}");
+        }
+
+        private static IntPtr GetObjectMemoryAddress(object obj)
+        {
+            var memoryLocator = GCHandle.Alloc(obj, GCHandleType.Pinned);
+            var stackAddress = memoryLocator.AddrOfPinnedObject();
+            memoryLocator.Free();
+            return stackAddress;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal class Point
+    {
+    }
+}
